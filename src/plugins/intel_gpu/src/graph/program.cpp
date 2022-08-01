1360f90964b764c2046c56b6991030f23c3c78b3
Mykola Dovbnia <mdovbnia@lohika.com>
2022-08-01 13:49:07 +0300

    [GPU] Added blocked layouts support for roi_pooling operations

--- src/plugins/intel_gpu/src/graph/program.cpp
+++ src/plugins/intel_gpu/src/graph/program.cpp
@@ -1431,10 +1431,12 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::adaptive_pooling::type_id() &&
             prim.type() != cldnn::bucketize::type_id() &&
             prim.type() != cldnn::roll::type_id() &&
+            prim.type() != cldnn::resample::type_id() &&
             prim.type() != cldnn::prior_box::type_id() &&
             prim.type() != cldnn::resample::type_id() &&
             prim.type() != cldnn::eye::type_id() &&
-            prim.type() != cldnn::generate_proposals::type_id()) {
+            prim.type() != cldnn::generate_proposals::type_id() &&
+            prim.type() != cldnn::roi_pooling::type_id()) {
             can_use_fsv16 = false;
         }
 
@@ -1471,7 +1473,8 @@ void program::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::resample::type_id() &&
             prim.type() != cldnn::prior_box::type_id() &&
             prim.type() != cldnn::eye::type_id() &&
-            prim.type() != cldnn::generate_proposals::type_id()) {
+            prim.type() != cldnn::generate_proposals::type_id() &&
+            prim.type() != cldnn::roi_pooling::type_id()) {
             can_use_bs_fs_yx_bsv16_fsv16 = false;
         }
     }
